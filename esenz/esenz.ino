5cd6f1057870a069e0a991c0c64585f0c18f28df
axcxl <axcxl.dev@gmail.com>
2020-04-22 23:03:07 +0300

    Firmware: major redesign - replace loop with reboot
    Replaced main loop with a sleep + a reboot. Hope this way the software
    is more stable, as I had random problems after a few hours. Also now the
    Wifi is mosly off (only on a few seconds to send info). Sleep time was
    extended to 10 minutes.

--- esenz/esenz.ino
+++ esenz/esenz.ino
@@ -1,5 +1,5 @@
 /*
- Basic ESP8266 MQTT example
+ Esenz ESP8266 MQTT example
 
  This sketch demonstrates the capabilities of the pubsub library in combination
  with the ESP8266 board/library.
@@ -37,9 +37,11 @@ const char* ssid = SSID;
 const char* password = PASS;
 const char* mqtt_server = SERV;
 
-#define DHTPIN 2     // Digital pin connected to the DHT sensor
-#define DHTTYPE DHT21   // DHT 21 (AM2301)
+//#define DHTPIN 4     // Digital pin 4 for Olimex ESPs
+#define DHTPIN 2       // Digital pin 2 for the rest
 
+//#define DHTTYPE DHT11   // Also have one of these
+#define DHTTYPE DHT21   // DHT 21 (AM2301)
 
 WiFiClient espClient;
 PubSubClient client(espClient);
@@ -48,12 +50,14 @@ DHT dht(DHTPIN, DHTTYPE);
 long lastMsg = 0;
 char msg[50];
 int value = 0;
-
 String id;
 
 void setup_wifi() {
+  Serial.println("Starting sensor connection");
+  dht.begin();  
 
-  delay(10);
+  delay(1000); //needed for sensor init
+  
   // We start by connecting to a WiFi network
   Serial.println();
   Serial.print("Connecting to ");
@@ -72,56 +76,19 @@ void setup_wifi() {
   Serial.println("WiFi connected");
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
-
-  Serial.println("Starting sensor connection");
-  dht.begin();  
-}
-
-void callback(char* topic, byte* payload, unsigned int length) {
-  Serial.print("Message arrived [");
-  Serial.print(topic);
-  Serial.print("] ");
-  for (int i = 0; i < length; i++) {
-    Serial.print((char)payload[i]);
-  }
-  Serial.println();
-
-  // Switch on the LED if an 1 was received as first character
-  if ((char)payload[0] == '1') {
-    digitalWrite(LED_BUILTIN, LOW);   // Turn the LED on (Note that LOW is the voltage level
-    // but actually the LED is on; this is because
-    // it is active low on the ESP-01)
-  } else {
-    digitalWrite(LED_BUILTIN, HIGH);  // Turn the LED off by making the voltage HIGH
-  }
-
 }
 
 void reconnect() {
   // Loop until we're reconnected
   while (!client.connected()) {
     Serial.print("Attempting MQTT connection...");
-    String mac = String(WiFi.macAddress());
-    char tmp[12];
-
-    // MAC format - FF:FF:FF:FF:FF:FF, remove the :
-    int j = 0;
-    for(int i = 9; i <= 16; i++) {
-      if(mac[i] != ':') {
-        tmp[j++] = mac[i];
-      }
-    }
-
-    id = String(tmp);
 
     Serial.print("Using ID: ");
     Serial.println(id);
     
     // Attempt to connect
     if (client.connect(id.c_str())) {
-      
       Serial.println("connected");
-      
     } else {
       Serial.print("failed, rc=");
       Serial.print(client.state());
@@ -137,7 +104,20 @@ void setup() {
   Serial.begin(115200);
   setup_wifi();
   client.setServer(mqtt_server, 1883);
-  client.setCallback(callback);
+
+  String mac = String(WiFi.macAddress());
+  char tmp[7];
+
+  // MAC format - FF:FF:FF:FF:FF:FF, remove the :
+  int j = 0;
+  for(int i = 9; i <= 16; i++) {
+    if(mac[i] != ':') {
+      tmp[j++] = mac[i];
+    }
+  }
+  tmp[j] = '\0'; //make sure string is terminated!
+
+  id = String(tmp);
 }
 
 void loop() {
@@ -147,34 +127,44 @@ void loop() {
   }
   client.loop();
 
-  long now = millis();
-  // Every 10 seconds
-  if (now - lastMsg > 10000) {
-    lastMsg = now;
-    
-    Serial.println("reading from sensor");
-    // Reading temperature or humidity takes about 250 milliseconds!
-    // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
-    float h = dht.readHumidity();
-    // Read temperature as Celsius (the default)
-    float t = dht.readTemperature();
-    Serial.print(F("Humidity: "));
-    Serial.print(h);
-    Serial.print(F("%  Temperature: "));
-    Serial.print(t);
-    Serial.println(F("°C "));
+  Serial.println("reading from sensor");
+  // Reading temperature or humidity takes about 250 milliseconds!
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  float h = dht.readHumidity();
+  // Read temperature as Celsius (the default)
+  float t = dht.readTemperature();
+  Serial.print(F(" Humidity: "));
+  Serial.print(h);
+  Serial.print(F("%  Temperature: "));
+  Serial.print(t);
+  Serial.println(F("°C "));
      
-    String temp = "data/temperature";
-    temp += '/';
-    temp += id;
-    temp += '\0';
+  String temp = "data/temperature";
+  temp += '/';
+  temp += id;
+  temp += '\0';
 
-    String hum = "data/humidity";
-    hum += '/';
-    hum += id;
-    hum += '\0';
-    
-    client.publish(temp.c_str(), String(t).c_str());
-    client.publish(hum.c_str(), String(h).c_str());
-  }
+  String hum = "data/humidity";
+  hum += '/';
+  hum += id;
+  hum += '\0';
+
+  Serial.println("Publishing");
+  client.publish(temp.c_str(), String(t).c_str());
+  client.publish(hum.c_str(), String(h).c_str());
+
+  delay(500); //needed to properly send te message
+
+  Serial.println("Turning off Wifi!");
+  // Turn off Wifi
+  WiFi.mode( WIFI_OFF );
+  WiFi.forceSleepBegin();
+  delay( 1 );
+
+  // Sleep for a while - 10 minutes should be ok - TOOD: make it configurable vis mqtt
+  delay( 600000 );
+
+  //then start over
+  Serial.println("Resetting!");
+  ESP.reset();
 }
